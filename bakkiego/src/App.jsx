b426f90c0b889f7c2cc3474ac383e890b2c1ae9e
Daniel <lekeayomidotun@gmail.com>
2024-02-23 21:31:56 +0200

    made the style for the home page and also made it responsive

--- bakkiego/src/App.jsx
+++ bakkiego/src/App.jsx
@@ -1,9 +1,8 @@
-import Home from'./components/home/Home'
+import Home from "./components/home/Home";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function App() {
-  return (
-    <Home />
-  );
+  return <Home />;
 }
 
 export default App;
--- bakkiego/src/components/home/Home.jsx
+++ bakkiego/src/components/home/Home.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import Logo from "../../assets/logo.jpg";
+import Movers from "../../assets/movers.jpg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import "./home.css";
 
 const Home = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <section className="container">
+      <div className="header">
+        <img src={Logo} alt="Bakkiego" className="logo_img" />
 
-export default Home
+        <nav className="navbar">
+          <a href="#introduction">Ride</a>
+          <a href="#drive">Drive</a>
+          <a href="#business">Business</a>
+          <a href="#about">About</a>
+
+          <FontAwesomeIcon icon={faBars} />
+        </nav>
+      </div>
+
+      <div className="container intro__panel">
+        <div className="right__panel">
+          <h2 className="motto">
+            Connecting you to your nearest delivery service
+          </h2>
+          <p className="motto_text">
+            Bakkiego is a delivery and haulage app designed to provide
+            individuals and businesses in South Africa with a flexible,
+            convenient, and affordable means of transporting their goods.
+          </p>
+
+          <div className="cta">
+            <a href="#download" download className="btn">
+              Get Bakkie Now
+            </a>
+            <a href="#business" className="btn btn-primary">
+              Earn Money
+            </a>
+          </div>
+        </div>
+          <img src={Movers} alt="Movers" className="panel__img" />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
