83740dacf9465dc2543bd6d2ca46740002e3aa73
Daniel <lekeayomidotun@gmail.com>
2024-02-29 11:40:38 +0200

    Updated Typscript for Router-Dom and added routes

--- bakkiego/src/index.js
+++ bakkiego/src/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
+import Home from "./components/home/Home";
+import Introduction from "./components/introduction/Introduction";
+import Services from "./components/services/Service";
+import Download from "./components/downloadApp/DownloadApp";
+import Contact from "./components/contact/Contact";
+import Business from "./components/business/Business";
+import Footer from "./components/footer/Footer";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route inde path="/" element={<Home />} />
+        <Route path="/introduction" element={<Introduction />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/downloadapp" element={<Download />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/bkfoot" element={<Footer />} />
+        <Route path='/business' element={<Business/>} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
 );
 
 
