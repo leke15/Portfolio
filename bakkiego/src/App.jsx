2ead37d9bcd5f36f691e2423876691e4acb37f01
Daniel <lekeayomidotun@gmail.com>
2024-03-11 21:06:15 +0200

    Finished services page and styles

--- bakkiego/src/App.jsx
+++ bakkiego/src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
-
+import Service from "./components/services/Service";
 const App = () => {
   return (
     <>
-    <Home />
-    <About />
+      <Home />
+      <About />
+      <Service />
     </>
   );
-}
+};
 
 export default App;
