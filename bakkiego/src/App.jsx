3d6c74dd748a3b68bdcef8788523d04fd7d79fbb
Daniel <lekeayomidotun@gmail.com>
2024-03-21 10:38:46 +0200

    Contact page and email.js service up and running

--- bakkiego/src/App.jsx
+++ bakkiego/src/App.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Service from "./components/services/Service";
+import DownloadApp from "./components/downloadApp/DownloadApp";
+import Contact from "./components/contact/Contact";
 const App = () => {
   return (
     <>
       <Home />
       <About />
       <Service />
+      <DownloadApp />
+      <Contact />
     </>
   );
 };
